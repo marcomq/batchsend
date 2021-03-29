c1914ada8770304f2654908d6499c067e3d69de7
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-03-29 09:59:57 +0200

    added sendcsv

---
+++ src/sendcsv.nim
@@ -0,0 +1,50 @@
+# bulkSend Library 
+# Copyright (C) 2021, by Marco Mengelkoch
+# Licensed under MIT License, see License file for more details
+# git clone https://github.com/marcomq/bulksend
+
+## reads csv file and sends it as xml to server
+## Important: Doesn't do perform XML or csv escapes!
+
+import parsecsv
+from os import paramStr, paramCount
+from streams import newFileStream
+import bulkSend
+
+proc sendCsv(csvFile: string) =
+  var httpHeader = "POST / HTTP/1.1\c\LHost: localhost\c\LConnection: keep-alive\c\LContent-Length: "
+  var s = newFileStream(csvFile, fmRead)
+  if s == nil:
+    raise newException(CatchableError, "Cannot open the file" & csvFile)
+
+  var parser: CsvParser
+  open(parser, s, paramStr(1))
+  parser.readHeaderRow()
+  if parser.headers.len == 0:
+    raise newException(CatchableError, "Cannot read csv header")
+  bulkSend.spawnTransmissionThread("localhost", port = 9292)
+
+  while readRow(parser):
+    var message: string
+    var i = 0
+    for val in items(parser.row):
+      if i < parser.headers.len:
+        message &= "<" & parser.headers[i] & ">" & val & "</" & parser.headers[i] & ">"
+        inc(i)
+    let httpMessage = httpHeader & $message.len & "\c\L\c\L" & message
+    bulkSend.pushMessage(httpMessage)
+  close(parser)
+  bulkSend.waitForTransmissionThread()
+
+proc main() =
+  try:
+    if paramCount() >= 1:
+      sendCsv(paramStr(1))
+    else:
+      raise newException(CatchableError, "You need to put a file as command line parameter") 
+  except:
+    echo "Error: " & getCurrentExceptionMsg()
+
+when isMainModule:
+  when system.appType != "lib":
+    main()
