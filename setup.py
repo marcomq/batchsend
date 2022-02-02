d766be4ac70bb5bf97d3b72790892f8a8dcea9fb
Marco Mengelkoch <Mmengelkoch@gmx.de>
2022-02-02 22:55:44 +0100

    increased version, changed versions

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 import os
 import sys
 from shutil import copy, rmtree
+import nimporter # needs to be installed first, when building from source
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -21,18 +22,9 @@ for index, fileName in enumerate(srcFiles):
 with open(targetDir + "/__init__.py", "w") as text_file:
     text_file.write("from batchsend.batchsend import *")
 
-def externalModules():
-    if not "sdist" in sys.argv:
-        import nimporter
-        return nimporter.build_nim_extensions(
-            danger=True, exclude_dirs=["test", "src"]
-        )
-    else:
-        return ""
-
 setup(
     name="batchsend",
-    version="0.3.4",
+    version="0.3.5",
     author="Marco Mengelkoch",
     author_email="MMengelkoch@gmx.de",
     scripts=["batchsend/__init__.py"],
@@ -49,10 +41,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
     ],
-    ext_modules=externalModules(),
+    ext_modules=nimporter.build_nim_extensions(danger=True, exclude_dirs=["test", "src"]),
     package_data={"": ["*.nim*"]},  # Distribute *.nim & *.nim.cfg source files
     # include_package_data=True,    # <- This line cannot work with package_data
-    setup_requires=["choosenim_install"],  # Optional. Auto-installs Nim compiler
-    install_requires=["nimporter"],  # Must depend on Nimporter
+    setup_requires=["choosenim_install"],  # auto-installs Nim compiler
+    install_requires=["nimporter"],  # required, but doesn't work as it is already imported here - you need to install it manually first
     packages=["batchsend"],
 )
